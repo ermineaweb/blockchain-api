956f2e3efc54f117535b0b0470024a6e2e49b0bc
ermineaweb <ermineaweb@gmail.com>
2023-07-07 23:01:11 +0200

    simplify

--- server/src/cmd/server.go
+++ server/src/cmd/server.go
@@ -1,22 +1,18 @@
 package main
 
 import (
-	"server/src/core/adapter"
-	"server/src/core/repository"
 	"server/src/core/service"
 	"server/src/infra/gin"
 	"server/src/infra/jsonrpc"
 )
 
 func main() {
-	var accountRepository repository.Account
-	accountRepository = jsonrpc.NewJsonRpc("http://localhost:8545")
 	// accountRepository = inmemory.InMemoryRepository{}
+	accountRepository := jsonrpc.NewJsonRpc("http://localhost:8545")
 
 	var accountService = service.NewAccountService(accountRepository)
 
-	var server adapter.Server
 	// server := mux.NewMuxServer("0.0.0.0:5000", *accountService)
-	server = gin.NewGinServer("0.0.0.0:5000", *accountService)
+	server := gin.NewGinServer("0.0.0.0:5000", *accountService)
 	server.Serve()
 }
